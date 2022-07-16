b0cb2c6f5b2c8d370fcc1b26f9d21c2ca7e158c4
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-15 23:39:00 -0300

    Add entity Sale. Create H2 application configs

---
+++ backend/src/main/java/com/rddev/dsmeta/entities/Sale.java
@@ -0,0 +1,26 @@
+package com.rddev.dsmeta.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+}
