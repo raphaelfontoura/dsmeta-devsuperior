a9af94994ea95156c8a2dab5d001074b43e0f215
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-15 22:22:05 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import icon from "../../assets/img/notification-icon.svg";
+
+function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notifier" />
+    </div>
+  )
+}
+
+export default NotificationButton
