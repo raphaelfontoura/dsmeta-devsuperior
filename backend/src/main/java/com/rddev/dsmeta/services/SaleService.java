81c48ddabe4a00ed76d12d1416443285259ec464
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-16 00:15:32 -0300

    Clean imports

--- backend/src/main/java/com/rddev/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/rddev/dsmeta/services/SaleService.java
@@ -3,7 +3,6 @@ package com.rddev.dsmeta.services;
 import com.rddev.dsmeta.entities.Sale;
 import com.rddev.dsmeta.repositories.SaleRepository;
 import lombok.RequiredArgsConstructor;
-import net.bytebuddy.asm.Advice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
