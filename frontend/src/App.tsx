ed71c6add5d16fe2209fb39e39a92f61b4be8a78
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-17 13:06:20 -0300

    Add toastify to notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import Header from "./components/Header";
-import NotificationButton from "./components/NotificationButton";
-import SalesCard from "./components/SalesCard";
+import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer } from 'react-toastify';
+
+import Header from './components/Header';
+import SalesCard from './components/SalesCard';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function NotificationButton({saleId} : Props) {
+
+  function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+      .then(response => {
+        toast.info("SMS enviado com sucesso!");
+      });
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notifier" />
     </div>
   )
